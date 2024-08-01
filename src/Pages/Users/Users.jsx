46108a7822fcf83c8623a08b2c8bf44fb4bee7d5
bilinacd <bilinacd99@gmail.com>
2024-08-01 09:24:54 +0200

    Omogućeno je dodavanje novih korisnika

--- src/Pages/Users/Users.jsx
+++ src/Pages/Users/Users.jsx
@@ -2,9 +2,29 @@ import React, { useState, useEffect } from "react";
 import UserCard from "../../components/UserCard/UserCard";
 import ResponsiveAppBar from "../../components/ResponsiveAppBar/ResponsiveAppBar";
 import { useUsers } from "../../hooks/services/useAuthentication";
-import { Grid, Box, Pagination, Typography } from "@mui/material";
-import { teal } from "@mui/material/colors";
-import { useParams } from "react-router-dom";
+import {
+  Grid,
+  Box,
+  Typography,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  FormHelperText,
+  Pagination,
+} from "@mui/material";
+import { useRoles } from "../../hooks/services/useRole";
+import { useAddUser } from "../../hooks/services/useAuthentication";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 
 const Users = () => {
   const { data: allUsersData = [] } = useUsers();
@@ -15,9 +35,86 @@ const Users = () => {
   const indexOfFirstUser = indexOfLastUser - usersPerPage;
   const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser);
   const totalPages = Math.ceil(users.length / usersPerPage);
+  const { data: rolesData } = useRoles();
+
+  const [open, setOpen] = useState(false);
+  const [errors, setErrors] = useState({});
+  const {
+    handleSubmit,
+    register,
+    reset,
+    setValue,
+    watch,
+    formState: { errors: formErrors },
+  } = useForm({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      username: "",
+      password: "",
+      role: "",
+      status: "active",
+    },
+  });
+
+  const formData = watch();
+
   const handlePageChange = (event, value) => {
     setCurrentPage(value);
   };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+    reset();
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const validateForm = () => {
+    const newErrors = {};
+
+    if (!formData.firstName) newErrors.firstName = "Ime je obavezno";
+    if (!formData.lastName) newErrors.lastName = "Prezime je obavezno";
+    if (!formData.email || !/\S+@\S+\.\S+/.test(formData.email))
+      newErrors.email = "Neispravna email adresa";
+    if (!formData.username) newErrors.username = "Korisničko ime je obavezno";
+    if (formData.username.length < 6)
+      newErrors.username = "Korisničko ime mora imati najmanje 6 karaktera";
+    if (
+      !formData.password ||
+      formData.password.length < 6 ||
+      !/[A-Z]/.test(formData.password) ||
+      !/[a-z]/.test(formData.password) ||
+      !/[0-9]/.test(formData.password)
+    )
+      newErrors.password =
+        "Lozinka mora imati najmanje 6 karaktera, uključujući broj, veliko i malo slovo";
+    if (!formData.role) newErrors.role = "Uloga je obavezna";
+    if (!formData.status) newErrors.status = "Status je obavezan";
+
+    return newErrors;
+  };
+
+  const onSubmit = (data) => {
+    const newErrors = validateForm();
+    if (Object.keys(newErrors).length === 0) {
+      mutateAdd(data, {
+        onSuccess: () => {
+          toast.success("Uspjesno ste dodali korisnika!");
+          handleClose();
+        },
+        onError: () =>
+          toast.error("Došlo je do greške prilikom dodavanja korisnika!"),
+      });
+    } else {
+      setErrors(newErrors); // Postavljanje grešaka u stanje
+    }
+  };
+
+  const { mutate: mutateAdd } = useAddUser();
 
   useEffect(() => {
     if (allUsersData) {
@@ -28,16 +125,33 @@ const Users = () => {
   return (
     <div>
       <ResponsiveAppBar />
-      <Typography
-        fontSize={30}
-        textAlign={"center"}
-        marginTop={5}
-        color="#1976d2"
+      <Box
+        sx={{
+          mt: 5,
+          px: 3,
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
       >
-        Informacije o korisnicima
-      </Typography>
+        <Typography
+          fontSize={30}
+          color="#1976d2"
+          sx={{ flexGrow: 1, textAlign: "center" }}
+        >
+          Informacije o korisnicima
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{ backgroundColor: "#1976d2" }}
+          onClick={handleClickOpen}
+        >
+          Dodaj korisnika
+        </Button>
+      </Box>
 
-      <Box sx={{ mt: 7, px: 3, pb: 5 }}>
+      <Box sx={{ mt: 3, px: 3, pb: 5 }}>
         {currentUsers.length > 0 ? (
           <>
             <Grid container spacing={3} justifyContent="center">
@@ -69,6 +183,126 @@ const Users = () => {
           <div>No users available</div>
         )}
       </Box>
+
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle color="#1976d2">Dodaj novog korisnika</DialogTitle>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="firstName"
+              name="firstName"
+              label="Ime"
+              type="text"
+              fullWidth
+              variant="outlined"
+              {...register("firstName", { required: true })}
+              error={!!errors.firstName}
+              helperText={errors.firstName}
+              autoComplete="given-name"
+            />
+            <TextField
+              margin="dense"
+              id="lastName"
+              name="lastName"
+              label="Prezime"
+              type="text"
+              fullWidth
+              variant="outlined"
+              {...register("lastName", { required: true })}
+              error={!!errors.lastName}
+              helperText={errors.lastName}
+              autoComplete="family-name"
+            />
+            <TextField
+              margin="dense"
+              id="email"
+              name="email"
+              label="Email"
+              type="email"
+              fullWidth
+              variant="outlined"
+              {...register("email", { required: true })}
+              error={!!errors.email}
+              helperText={errors.email}
+              autoComplete="email"
+            />
+            <TextField
+              margin="dense"
+              id="username"
+              name="username"
+              label="Korisničko ime"
+              type="text"
+              fullWidth
+              variant="outlined"
+              {...register("username", { required: true })}
+              error={!!errors.username}
+              helperText={errors.username}
+              autoComplete="username"
+            />
+            <TextField
+              margin="dense"
+              id="password"
+              name="password"
+              label="Lozinka"
+              type="password"
+              fullWidth
+              variant="outlined"
+              {...register("password", { required: true })}
+              error={!!errors.password}
+              helperText={errors.password}
+              autoComplete="current-password"
+            />
+            <TextField
+              select
+              margin="dense"
+              id="role"
+              name="role"
+              label="Uloga"
+              fullWidth
+              variant="outlined"
+              value={formData.role || ""}
+              onChange={(e) => setValue("role", e.target.value)}
+              error={!!errors.role}
+              helperText={errors.role}
+            >
+              {rolesData?.map((role) => (
+                <MenuItem value={role.previewName} key={`role-${role.roleId}`}>
+                  {role.previewName}
+                </MenuItem>
+              ))}
+            </TextField>
+            <FormControl component="fieldset" margin="dense">
+              <FormLabel component="legend">Status</FormLabel>
+              <RadioGroup
+                aria-label="status"
+                name="status"
+                value={formData.status}
+                onChange={(e) => setValue("status", e.target.value)}
+              >
+                <FormControlLabel
+                  value="active"
+                  control={<Radio />}
+                  label="Aktivan"
+                />
+                <FormControlLabel
+                  value="inactive"
+                  control={<Radio />}
+                  label="Neaktivan"
+                />
+              </RadioGroup>
+              {errors.status && (
+                <FormHelperText error>{errors.status}</FormHelperText>
+              )}
+            </FormControl>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Otkaži</Button>
+            <Button type="submit">Sačuvaj</Button>
+          </DialogActions>
+        </form>
+      </Dialog>
     </div>
   );
 };
